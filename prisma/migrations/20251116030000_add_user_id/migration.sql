e6f8c015182ab67e59273a2405c9fb9f7f762ba3
yunbow <yunbow365@gmail.com>
2026-05-02 18:07:18 +0900

    feat(infra): TICKET-0001 PostgreSQL docker-compose追加 / prisma schema PostgreSQL化 / migrations削除

--- prisma/migrations/20251116030000_add_user_id/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN "userId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "users_userId_key" ON "users"("userId");
-
--- CreateIndex
-CREATE INDEX "users_userId_idx" ON "users"("userId");
